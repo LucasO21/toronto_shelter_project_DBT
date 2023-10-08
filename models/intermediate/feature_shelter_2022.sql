0e6cc4aaf8c11ee69fade9e7b61fa98443d045ae
Lucas Okwudishu <clfo2014@gmail.com>
2023-10-08 17:34:59 +0000

    add additional variables in cohort models

--- models/intermediate/feature_shelter_2022.sql
+++ models/intermediate/feature_shelter_2022.sql
@@ -12,11 +12,15 @@ with
             , t2.model_cohort    
         from {{ ref ('clean_shelter_2022') }} as t1
         left join {{ref ('model_cohort_flag_2022') }} as t2
-        on t1.organization_name     = t2.organization_name
-            and t1.shelter_group    = t2.shelter_group
-            and t1.location_name    = t2.location_name
-            and t1.location_address = t2.location_address
-            and t1.location_city    = t2.location_city
+        on t1.organization_name           = t2.organization_name
+            and t1.shelter_group          = t2.shelter_group
+            and t1.program_id             = t2.program_id
+            and t1.program_area           = t2.program_area
+            and t1.sector                 = t2.sector
+            and t1.program_model          = t2.program_model
+            and t1.overnight_service_type = t2.overnight_service_type
+            and t1.capacity_type          = t2.capacity_type
+            and t1.location_id            = t2.location_id
     )
 
     , cte_2 as (
@@ -38,3 +42,5 @@ with
         select
             * except (model_cohort)
         from cte_2
+
+
