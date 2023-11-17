4774e078a6053c46819cc84c5babf7ce5c086e14
Lucas Okwudishu <clfo2014@gmail.com>
2023-11-17 03:43:01 +0000

    update: fix issue with api data not updating, fix duplication in reporting

--- models/prediction/shelter_occupancy_prediction_distinct.sql
+++ models/prediction/shelter_occupancy_prediction_distinct.sql
@@ -9,7 +9,26 @@ This model dedupes future predictions
     )
 }}
 
-select
-    distinct
-        *
-from {{source('data_pred', 'shelter_occupancy_predictions')}}
+with 
+    distinct_pred as (
+        select
+            distinct
+                *
+        from {{source('data_pred', 'shelter_occupancy_predictions')}}
+
+    )
+
+    , distinct_pred_date_format as (
+      select
+          *
+        , parse_datetime("%F %I:%M %p", pred_time) as pred_time_dt
+      from distinct_pred
+  )
+  
+  , distinct_pred_rank as (
+      select
+          *
+        , rank() over (partition by pkey order by pred_time_dt desc) as pred_rank
+      from distinct_pred_date_format
+  )
+    select * from distinct_pred_rank
